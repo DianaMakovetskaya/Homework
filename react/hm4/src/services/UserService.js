4bbe31e8203fdc35db2266e34cee13cb039d3ea6
Diana <diana.makovetskaya1812@gmail.com>
2020-12-10 14:10:28 +0200

    React hm-4

--- react/hm4/src/services/UserService.js
+++ react/hm4/src/services/UserService.js
@@ -3,8 +3,6 @@ export class UserService{
     async getAllUsers(){
         return await fetch(this.url).then(value => value.json())
     }
-    async getOneUser(path){
-        return await fetch(`https://jsonplaceholder.typicode.com${path}`).then(value => value.json())
-    }
+
 
 }
