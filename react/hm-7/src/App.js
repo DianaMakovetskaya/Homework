22d793a2f0209475071cddc4cdf686cc03db49f6
Diana <diana.makovetskaya1812@gmail.com>
2021-01-05 15:47:33 +0200

    React hm-7 ))

--- react/hm-7/src/App.js
+++ react/hm-7/src/App.js
@@ -63,6 +63,9 @@ export default function App(){
                 //     </>
                 // )
 
+
+
+
                 state&&(
                     <><br/>
                     Name: {state.name}
